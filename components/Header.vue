eb95a45a1fa0ea7f25d1c6d1c7173caaed015f9e
zhanggangbin <1738238581@qq.com>
2022-05-22 09:55:33 +0800

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtHeader',
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
